66928b2458f005fbb7768e3cb7320c7b42adae49
Glitch (hello-webpage) <none>
2020-05-06 01:34:47 +0000

    🐦🍨 Checkpoint ./script.js:587074/338 ./index.html:587074/591 ./style.css:587074/168

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+function setup(){
+  // Code here runs only once
+  createCanvas(800, 600)
+}
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function draw(){
+  // Code here runs continuously
+  background(220)
+
+  // ellipse(50, 50, 50, 50)
+
+}
