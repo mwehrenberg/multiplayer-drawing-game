482bdd5e75b8432c8c7dbd7f8312bcc8b31f39f7
Glitch (cssi-may-7-sample-lesson) <none>
2020-07-06 05:12:36 +0000

    🏅🌛 Checkpoint ./script.js copy:16055627/309 ./script2.js:16055627/5

---
+++ script2.js
@@ -0,0 +1,15 @@
+// Content behind double slashes is a comment. Use it for plain English notes,
+// or for code that you want to temporarily disable.
+
+function setup(){
+  // Code here runs only once
+  createCanvas(800, 600)
+}
+
+function draw(){
+  // Code here runs continuously
+  background(220)
+
+  //ellipse(50, 50, 50, 50)
+
+}
