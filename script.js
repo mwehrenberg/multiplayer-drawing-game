520eb0fdf21155586ce7b6118f697d9ba970f443
Glitch (cssi-may-7-sample-lesson) <none>
2020-07-07 18:14:31 +0000

    🐿⛑ Checkpoint ./script.js:18580012/100

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ function setup(){
   createCanvas(800, 600);
   // We only want to load the logo once.
   dvdImage = loadImage("https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387");
+  x = 50;
+  
+  x += 5;
+  
+  x += 1;
+  x++;
 }
 
 function draw(){
   background(220);
   // Draw the logo at the new position.
-  image(dvdImage, 50, 50, 200, 150);
+  image(dvdImage, x, 50, 200, 150);
 }
