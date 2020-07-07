23882a441d242e749208928bcaf5a01f791fcca7
Glitch (cssi-may-7-sample-lesson) <none>
2020-07-07 18:26:28 +0000

    🔑⛳️ Checkpoint ./script.js:18580012/345

--- script.js
+++ script.js
@@ -5,15 +5,28 @@ function setup(){
   // We only want to load the logo once.
   dvdImage = loadImage("https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387");
   x = 50;
-  
-  x += 5;
-  
-  x += 1;
-  x++;
+  xVelocity = 3;
 }
 
 function draw(){
   background(220);
+  
+  if (x == 50) {
+    
+  }
+  if (x == 75) {
+    
+  }
+  
+  
+  if (x < 600) {
+      x += xVelocity
+  } else if (x > 600) {
+    
+  } else {
+    
+  }
+  
   // Draw the logo at the new position.
   image(dvdImage, x, 50, 200, 150);
 }
