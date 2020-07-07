6e21617f5f3d476238ef267cbe9544179a28beb2
Glitch (cssi-may-7-sample-lesson) <none>
2020-07-07 18:38:26 +0000

    💐🙆 Checkpoint ./script.js:18580012/486

--- script.js
+++ script.js
@@ -5,27 +5,29 @@ function setup(){
   // We only want to load the logo once.
   dvdImage = loadImage("https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387");
   x = 50;
+  y = 50;
   xVelocity = 3;
+  yVelocity = 3;
 }
 
 function draw(){
   background(220);
   
-  if (x == 50) {
-    
+  if (x > 600) {
+    // If the DVD is too far to the right, move left
+    xVelocity = -3
+  } else if (x < 0) {
+    // If the DVD is too far to the left, move right
+    xVelocity = 3
   }
-  if (x == 75) {
-    
-  }
+  x += xVelocity
   
-  
-  if (x < 600) {
-      x += xVelocity
-  } else if (x > 600) {
-    
-  } else {
-    
+  if () {
+    // If the DVD is too far down, move up
+  } else if () {
+    // If the DVD is too far up, move down
   }
+  y += yVelocity
   
   // Draw the logo at the new position.
   image(dvdImage, x, 50, 200, 150);
