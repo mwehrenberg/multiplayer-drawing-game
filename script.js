474b219c541ea8385a98f0e6c5aee928af892d65
Glitch (cssi-may-7-sample-lesson) <none>
2020-07-09 18:31:20 +0000

    🍴⏳ Checkpoint ./script.js:18580012/608

--- script.js
+++ script.js
@@ -15,15 +15,38 @@ function setup() {
   brushHue = 0;
   strokeWeight(6);
   background(95);
+  prevX = 0;
+  prevY = 0;
 }
 
 function draw() {
   //background(95);
+  
   chooseColors();
-  rect(mouseX, mouseY, 15, 15);
+  if (mouseIsPressed) {
+    //rect(mouseX, mouseY, 15, 15);
+    line(prevX, prevY, mouseX, mouseY);
+  }
+  prevX = mouseX
+  prevY = mouseY
 }
 
 function chooseColors() {
+  brushHue += 1
+  if (brushHue >= 360) {
+    brushHue = 0
+  }
   stroke(brushHue, 50, 80);
   fill(brushHue, 50, 80);
 }
+
+function keyPressed() {
+  background(95);
+}
+
+/*
+function mousePressed() {
+  // x, y, width, height
+  ellipse(random(width), random(height), 30, 30);
+}
+*/
