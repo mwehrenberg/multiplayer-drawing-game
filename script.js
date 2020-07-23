d8266c6990c8806bad015320a440b4de5ac8e427
Glitch (cssi-may-7-sample-lesson) <none>
2020-07-23 05:29:43 +0000

    🐋🚉 Checkpoint ./script.js:18580012/1343

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-/* global createCanvas, colorMode, strokeWeight, background, rect, stroke, fill,
-HSB, mouseX, mouseY */
-
-/* this is a
-multi-line comment */
-
-// This is a single-line comment.
+/* global createCanvas, colorMode, HSB, strokeWeight, background, rect, mouseX,
+mouseY, stroke, fill */
 
 let brushHue
 
@@ -14,39 +9,15 @@ function setup() {
   colorMode(HSB, 360, 100, 100);
   brushHue = 0;
   strokeWeight(6);
-  background(95);
-  prevX = 0;
-  prevY = 0;
 }
 
 function draw() {
-  //background(95);
-  
+  background(95);
   chooseColors();
-  if (mouseIsPressed) {
-    //rect(mouseX, mouseY, 15, 15);
-    line(prevX, prevY, mouseX, mouseY);
-  }
-  prevX = mouseX
-  prevY = mouseY
+  rect(mouseX, mouseY, 15, 15);
 }
 
 function chooseColors() {
-  brushHue += 1
-  if (brushHue >= 360) {
-    brushHue = 0
-  }
   stroke(brushHue, 50, 80);
   fill(brushHue, 50, 80);
 }
-
-function keyPressed() {
-  background(95);
-}
-
-/*
-function mousePressed() {
-  // x, y, width, height
-  ellipse(random(width), random(height), 30, 30);
-}
-*/
